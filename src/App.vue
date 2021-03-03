a2e822f0c8be9c02bd1db707955873c9b7615be2
Minh Duc Nguyen <nguyenminhducci@gmail.com>
2021-03-03 16:18:23 +0100

    form and button added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <h1>Pokémon Guesser</h1>
       <p v-if="!imgSrc">WHERE IS IMAGE</p>
       <b-img :src="imgSrc"></b-img>
-      <p>{{ name }}</p>
+      <b-form-input v-model="guess" placeholder="Guess the pokémon"></b-form-input>
+      <b-button @click="fetchRandomPokemon">New Pokémon</b-button>
     </b-container>
   </div>
 </template>
@@ -14,31 +15,49 @@ export default {
   name: "App",
   data() {
     return {
-      name: '',
-      filename: '',
-    }
+      name: "",
+      guess: "",
+      filename: "",
+      obf: 1,
+    };
   },
   computed: {
     imgSrc() {
-      return 'http://localhost:3000/' + this.filename;
-    }
+      return "http://localhost:3000/" + this.filename;
+    },
+
   },
   methods: {
     async fetchPokemon(id, obf) {
-      const response = await fetch(`http://localhost:3000/pokemon/${id}/${obf}`);
+      const response = await fetch(
+        `http://localhost:3000/pokemon/${id}/${obf}`
+      );
       const responseData = await response.json();
       if (!response.ok) {
-        const error = new Error(responseData.message || 'Failed to fetch!');
+        const error = new Error(responseData.message || "Failed to fetch!");
         throw error;
       }
-      this.name = responseData['name_en'];
-      this.filename = responseData['filename'];
-
-    }
+      console.log("neu");
+      this.name = responseData["name_en"];
+      this.filename = responseData["filename"];
+    },
+    fetchRandomPokemon() {
+      this.obf = 6;
+      const id = Math.floor(Math.random() * 151) + 1;
+      this.fetchPokemon(id, this.obf);
+    },
+    sleep(milliseconds) {
+      const date = Date.now();
+      let currentDate = null;
+      do {
+        currentDate = Date.now();
+      } while (currentDate - date < milliseconds);
+    },
   },
+
   created() {
-    this.fetchPokemon(1, 1);
-  }
+    this.fetchRandomPokemon();
+  },
 };
 </script>
 
