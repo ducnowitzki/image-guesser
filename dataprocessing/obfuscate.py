fd3887bf0484fb6a59549ece6c64b3679a560c2f
Cihad <cihadcolak@gmail.com>
2021-04-19 17:41:28 +0200

    change script to resize original image too

--- dataprocessing/obfuscate.py
+++ dataprocessing/obfuscate.py
@@ -10,6 +10,7 @@ steps = [0,2, 4,6,8,10,12,24,32]
 for i in range(1,891):
 	print("current step:" + str(i) + "/890" )
 	og_im = Image.open("./modified_images/" + str(i) + ".png")
+	og_im = og_im.resize((256,256), resample=Image.BOX)
 	og_im.save("../static/obf/"  +  outerObject[str(i)]["filenames"]["filename"])
 	for j in range(1,9):
 		size = steps[j], steps[j]
