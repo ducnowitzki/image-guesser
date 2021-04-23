f90cc59903cd61eb822826ee17676eff528a0356
Minh Duc Nguyen <nguyenminhducci@gmail.com>
2021-04-23 14:54:36 +0200

    added focus to textfield on each fetch, fetc when mounted

--- config/express.js
+++ config/express.js
@@ -24,34 +24,6 @@ module.exports = function() {
 
   var pokemonMiddleware = require('../app/routes/pokemon.server.routes.js')
   pokemonMiddleware(app);
-//   app.use("/img", (req, res, next) => {
-//     var options = {
-//       // Alt: process.cwd
-//       root: path.resolve(__dirname + "/../static"),
-//     };
-
-//     // Alt: image streaming
-//     res.sendFile("pikachu_test.png", options, (err) => {
-//       if (err) next(err);
-//       else console.log("Sent: pikachu");
-//     });
-//     next();
-
-//     // app.get('/report/:chart_id/:user_id',(req, res) => {
-//     //     const r = fs.createReadStream('path to file') // or any other way to get a readable stream
-//     //     const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
-//     //     stream.pipeline(
-//     //      r,
-//     //      ps, // <---- this makes a trick with stream error handling
-//     //      (err) => {
-//     //       if (err) {
-//     //         console.log(err) // No such file or any other kind of error
-//     //         return res.sendStatus(400);
-//     //       }
-//     //     })
-//     //     ps.pipe(res) // <---- this makes a trick with stream error handling
-//     //   })
-//   });
 
   return app;
 };
